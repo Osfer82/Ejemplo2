20c45afd6164776ac7d2d30961b940b0f2a8aa35
PC <PC@PC-PC>
2017-02-06 12:05:06 -0500

    Primer commit

---
+++ JavaApplication4/src/GitHub_1/CuadrosDialogo.java
@@ -0,0 +1,24 @@
+package GitHub_1;
+
+import javax.swing.JOptionPane;
+
+public class CuadrosDialogo {
+
+    public static void main(String[] args) {
+        
+        String mate;
+        String fisica;
+        String quimica;
+        String progra;
+        String nombre;
+        
+        double n1, n2, n3, n4, promedio;
+        
+        JOptionPane.showMessageDialog(null, "Calcular el Promedio de Notas");
+        nombre=JOptionPane.showInputDialog("Escriba su nombre");
+        JOptionPane.showMessageDialog(null, nombre);
+        
+        
+        
+    }//Fmain
+}//Fclass
